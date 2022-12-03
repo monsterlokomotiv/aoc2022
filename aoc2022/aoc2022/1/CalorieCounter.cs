1425d05767ed552db6cd8f7e456fb6b69e8eaa70
monsterlokomotiv <Pshycoone92@hotmail.com>
2022-12-03 12:55:02 +0100

    Committing again

---
+++ aoc2022/aoc2022/1/CalorieCounter.cs
@@ -0,0 +1,64 @@
+﻿namespace aoc2022._1
+{
+    internal class CalorieCounter
+    {
+        private readonly string _inputFilePath;
+
+        public CalorieCounter()
+        {
+            _inputFilePath = Directory.GetCurrentDirectory() + "\\1\\1_input.txt";
+        }
+
+        public int CalculateElfWithMaxCalories()
+        {
+            var maxCalorieCount = 0;
+            foreach(var calorieCount in GetCalorieCountFromFile(_inputFilePath))
+            {
+                if(calorieCount > maxCalorieCount)
+                    maxCalorieCount = calorieCount;
+            }
+
+            return maxCalorieCount;
+        }
+
+        public int CalculateSumOfTopThreeCalorieCount()
+        {
+            var maxCalories = new int[3];
+            Array.Fill(maxCalories, 0);
+            
+            foreach (var parsedCalorieCount in GetCalorieCountFromFile(_inputFilePath))
+            {
+                ReplaceLowestValueIfLower(maxCalories, parsedCalorieCount);
+            }
+            
+            return maxCalories.Sum();
+        }
+
+        private static void ReplaceLowestValueIfLower(int[] maxCalories, int newCalorieCount)
+        {
+            if (newCalorieCount < maxCalories[0])
+                return;
+
+            maxCalories[0] = newCalorieCount;
+            //With sorting we can always guarantee that the smallest is first
+            //Should be faster to do sort every now and then compared to always iterating the three elements?
+            Array.Sort(maxCalories);
+        }
+
+        private static IEnumerable<int> GetCalorieCountFromFile(string filePathAndName)
+        {
+            var calorieCount = 0;
+            foreach(var line in File.ReadLines(filePathAndName))
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    yield return calorieCount;
+                    calorieCount = 0;
+                }
+
+                if (int.TryParse(line, out int parsedCalorieCount))
+                    calorieCount += parsedCalorieCount;
+            }
+        }
+    }
+}
---
+++ aoc2022/aoc2022/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using aoc2022._1;
+
+Console.WriteLine("Hello, World!");
+
+var calorieCounter = new CalorieCounter();
+Console.WriteLine(calorieCounter.CalculateSumOfTopThreeCalorieCount());
+Console.ReadLine();
